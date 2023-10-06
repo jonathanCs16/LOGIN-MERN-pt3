6c8540749afe18627880b140c27edc720b1c8e81
jonathanCs16 <jonathansanchez1612@gmail.com>
2023-10-06 12:55:29 -0500

    video 3 terminado

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import  express  from "express";
+import morgan from "morgan";
+import authRoutes from './routes/auth.routes.js'; 
+import taskRouter from './routes/task.routes.js'
+import cookieParser from "cookie-parser";
+
+const app = express(); 
+app.use(express.json()); 
+app.use(morgan('dev')); 
+app.use(cookieParser()); 
+app.use("/api", authRoutes);
+app.use("/api", taskRouter);
+
+
+export default app; 
