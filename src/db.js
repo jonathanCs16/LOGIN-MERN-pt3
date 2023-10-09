3776a206c4ef3dd7b19a5627ea70bcd226337bca
Jonathan Chiquito <jonathansanchez1612@gmail.com>
2023-10-09 14:15:43 -0500

    video 3 terminado, con mi mongo

--- src/db.js
+++ src/db.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-       await mongoose.connect("mongodb+srv://jdgm1038:jose123@cluster0.903bhsv.mongodb.net/"); 
+       await mongoose.connect("mongodb+srv://jonathansanchez1612:jesus1612@cluster0.jr16hm9.mongodb.net/"); 
        console.log(">> DB Connect "); 
     } catch (error) {
         console.log(error)
     }
-}
+};
